763ba8724695c97397871c5967d4ffa8ed6a76f3
BishalParajuli4 <124025748+BishalParajuli4@users.noreply.github.com>
2025-01-31 15:31:07 +0545

    Creating new project and doing setup

---
+++ Hamro_Bazar/src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-amber-400 text-black">My App</h1>
+      <button type="button" onClick={""}>
+        Click me!
+      </button>
+    </div>
+  );
+};
+export default App;
