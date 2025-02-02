f1cca04f556e4bd75bc5413772472d0c2fb6e112
BishalParajuli4 <124025748+BishalParajuli4@users.noreply.github.com>
2025-02-02 13:05:30 +0545

    Cart creation

---
+++ Hamro_Bazar/src/Forms/cart.jsx
@@ -0,0 +1,13 @@
+import Card from "../components/card";
+
+const Cart = () => {
+  return (
+    <div>
+      <div className="cart">
+        <h1>Cart</h1>
+      </div>
+      <Card />
+    </div>
+  );
+};
+export default Cart;
